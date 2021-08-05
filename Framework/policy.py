e9ddc2ac99903e7d7ed291108ce12f5315baaa27
Vikas Munshi <vikas.munshiW@gmail.com>
2021-08-05 15:00:03 +0200

    added policy composition logic

--- Framework/policy.py
+++ Framework/policy.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 from __future__ import print_function
 
 from dataclasses import asdict, dataclass
-from functools import cache, cached_property
+from functools import cached_property, lru_cache, reduce
 from json import JSONDecodeError, dump, load
 from pathlib import Path
 from typing import Dict, Generator, Optional, Set, Tuple, Type, TypeVar
@@ -52,6 +52,11 @@ class BasePolicy:
             dump(policy_data, out_file, indent=4)
 
     @classmethod
+    def from_dict(cls: Type[T], data: dict) -> T:
+        policy_type = {c.__name__: c for c in BasePolicy.__subclasses__()}[data['type']]
+        return policy_type(**policy_type.param_map(data))
+
+    @classmethod
     def load(cls: Type[T], file: Path, fix: bool = False) -> Optional[T]:
         with open(file) as in_file:
             try:
@@ -60,14 +65,16 @@ class BasePolicy:
                 return
         if fix and policy_data['namespace'] != file.parent.stem:
             policy_data['namespace'] = file.parent.stem
-        policy_type = {c.__name__: c for c in BasePolicy.__subclasses__()}[policy_data['type']]
-        obj = policy_type(**policy_type.param_map(policy_data))
+        try:
+            obj = cls.from_dict(policy_data)
+        except AttributeError:
+            return
         if fix and (policy_data.get('id') != obj.id or policy_data['namespace'] != file.parent.stem):
             obj.dump(file)
         return obj
 
     @staticmethod
-    @cache
+    @lru_cache()
     def policy_map(path: Path = None, fix: bool = False) -> Dict[str, T]:
         def ls_dir(dir_path: Path) -> Generator[Path]:
             for file in dir_path.iterdir():
@@ -99,6 +106,7 @@ class Policy(BasePolicy):
     allowed: FrozenDict[str, Tuple[str]]
     blocked: FrozenDict[str, Tuple[str]]
     enforced: FrozenDict[str, str]
+    required: Tuple[str]
     possible: Tuple[str]
 
     @staticmethod
@@ -106,11 +114,12 @@ class Policy(BasePolicy):
         return dict(BasePolicy.param_map(data), **{'allowed': FrozenDict(data.get('allowed', {})),
                                                    'blocked': FrozenDict(data.get('blocked', {})),
                                                    'enforced': FrozenDict(data.get('enforced', {})),
+                                                   'required': tuple(data.get('required', [])),
                                                    'possible': tuple(data.get('possible', [])), })
 
     @cached_property
     def all_params(self) -> Set[str]:
-        return set(*(self.allowed.keys()), *(self.blocked.keys()), *(self.enforced.keys()))
+        return set(tuple(self.allowed.keys()) + tuple(self.blocked.keys()) + tuple(self.enforced.keys()))
 
     @cached_property
     def is_consistent(self) -> bool:
@@ -118,19 +127,58 @@ class Policy(BasePolicy):
             return False
         if self.enforced and not self.validate(assigned=self.enforced):
             return False
-        if self.blocked and any(value in self.allowed.get(param, tuple()) for param, value in self.blocked.items()):
+        if self.blocked and any(any(value in self.allowed.get(param, tuple()) for value in values)
+                                for param, values in self.blocked.items()):
             return False
         return True
 
+    def __post_init__(self):
+        if not self.is_consistent:
+            c = '; '.join('{}: {}'.format(a, getattr(self, a)) for a in ('allowed', 'blocked', 'enforced', 'possible'))
+            raise PolicyViolation('Policy {} ({}) is inconsistent; {}'.format(self.id, self.name, c))
+
     def __add__(self: Policy, other: Policy) -> Policy:
-        policy_data = asdict(self)
-        # addition logic here
-        return Policy(**policy_data)
+        if isinstance(self, Policy) and isinstance(other, PolicyExemption):
+            return self - other
+        if not (isinstance(self, Policy) and isinstance(other, Policy)):
+            raise PolicyViolation('Cannot combine type {} with {}'.format(type(self).name, type(other).name))
+        if self.target != other.target:
+            raise PolicyViolation('Cannot combine target {} with {}'.format(self.target, other.target))
+        if ev := [param for param, value in self.enforced.items() if value != other.enforced.get(param, value)]:
+            raise PolicyViolation('inconsistent values for enforced Parameters: {}'.format(', '.join(ev)))
+
+        return Policy.from_dict(
+            dict(name='{}:{} combined with {}:{}'.format(self.id, self.name, other.id, other.name),
+                 version='{}:{} {}:{}'.format(self.id, self.version, other.id, other.version),
+                 doc='{}:{} {}:{}'.format(self.id, self.doc, other.id, other.doc), target=self.target,
+                 namespace='{}:{} {}:{}'.format(self.id, self.namespace, other.id, other.namespace), type=self.type,
+                 allowed={param: (lambda l1, l2: list(set(l1 or l2).intersection(set(l2 or l1))))(
+                     self.allowed.get(param), other.allowed.get(param))
+                     for param in set(list(self.allowed.keys()) + list(other.allowed.keys()))},
+                 blocked={param: list(set(self.blocked.get(param, []) + other.blocked.get(param, [])))
+                          for param in set(list(self.blocked.keys()) + list(other.blocked.keys()))},
+                 enforced=dict(other.enforced, **self.enforced), required=list(set(self.required + other.required)),
+                 possible=[] if not (self.possible and other.possible) else list(set(self.possible + other.possible))))
 
     def __sub__(self: Policy, other: PolicyExemption) -> Policy:
-        policy_data = asdict(self)
-        # exemption logic here
-        return Policy(**policy_data)
+        if (not isinstance(self, Policy)) or (not isinstance(other, PolicyExemption)):
+            raise PolicyViolation('Cannot exempt type {} by {}'.format(type(self).name, type(other).name))
+        if self.target != other.target:
+            raise PolicyViolation('Cannot exempt target {} with {}'.format(self.target, other.target))
+
+        return Policy.from_dict(
+            dict(name='{}:{} with exemptions {}:{}'.format(self.id, self.name, other.id, other.name),
+                 version='{}:{} {}:{}'.format(self.id, self.version, other.id, other.version),
+                 doc='{}:{} {}:{}'.format(self.id, self.doc, other.id, other.doc), target=self.target,
+                 namespace='{}:{} {}:{}'.format(self.id, self.namespace, other.id, other.namespace), type=self.type,
+                 allowed={param: list(set(self.allowed.get(param, []) + other.exempted.get(param, [])))
+                          for param in set(list(self.allowed.keys()) + list(other.exempted.keys()))},
+                 blocked={param: list(value for value in values if value not in other.exempted.get(param, []))
+                          for param, values in self.blocked.items()},
+                 enforced={param: value for param, value in self.enforced.items() if
+                           value != other.exempted.get(param)},
+                 required=list(set(self.required) - set(other.exempted.keys())),
+                 possible=list(set(list(self.possible) + list(other.exempted.keys()))) if self.possible else []))
 
     def apply_policy(self: Policy, policy: Policy) -> Policy:
         return self + policy
@@ -180,23 +228,48 @@ class PolicySet(BasePolicy):
                                                    'exemptions': tuple(data.get('exemptions', [])), })
 
     @cached_property
-    def policy(self) -> FrozenDict[str, Policy]:
-        return FrozenDict({})
+    def policy(self) -> Policy:
+        policies = [Policy.get_policy_by_id(policy_id) for policy_id in self.policies]
+        exemptions = [Policy.get_policy_by_id(policy_id) for policy_id in self.exemptions]
+        return reduce(lambda x, y: x - y, exemptions, reduce(lambda x, y: x + y, policies))
 
 
 @dataclass(frozen=True)
 class Config(BasePolicy):
     assigned: FrozenDict[str, FrozenDict[str, str]]
-    applicable: FrozenDict[str, Tuple[str]]
+    applicable: Tuple[str]
 
     @staticmethod
     def param_map(data: dict) -> dict:
         return dict(BasePolicy.param_map(data), **{'assigned': FrozenDict(data.get('assigned', {})),
-                                                   'applicable': FrozenDict(data.get('applicable', {})), })
+                                                   'applicable': tuple(data.get('applicable', tuple())), })
 
     @cached_property
     def policy(self) -> FrozenDict[str, Policy]:
-        return FrozenDict({})
+        policies, exemptions = {}, {}
+        for policy in (BasePolicy.get_policy_by_id(policy_id) for policy_id in self.applicable):
+            if isinstance(policy, Policy):
+                policies.setdefault(policy.target, [])
+                policies[policy.target] += [policy]
+            elif isinstance(policy, PolicySet):
+                policies.setdefault(policy.policy.target, [])
+                policies[policy.target] += [policy.policy]
+            elif isinstance(policy, PolicyExemption):
+                exemptions.setdefault(policy.target, [])
+                exemptions[policy.target] += [policy]
+            else:
+                print('unknown type'.format(type(policy).name))
+
+        return FrozenDict({target: PolicySet.from_dict(dict(
+            name=self.name,
+            version=self.version,
+            doc=self.doc,
+            target=target,
+            namespace=self.namespace,
+            type='PolicySet',
+            policies=policies[target],
+            exemptions=exemptions.get(target, tuple()),
+        )).policy for target in policies.keys()})
 
     @cached_property
     def config(self) -> FrozenDict[str, FrozenDict[str, str]]:
