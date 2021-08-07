22f35ccd8f96f8f2ebc51f41eab3eb0bfe895cb3
Vikas Munshi <vikas.munshi@gmail.com>
2021-08-07 14:49:42 +0200

    fixed using setup.cfg for install

--- codifiednorms/__init__.py
+++ codifiednorms/__init__.py
@@ -4,6 +4,5 @@ try:
     from .policy import *
 except ImportError:
     from policy import *
-__package__ = 'codifiednorms'
-__version__ = '0.1.20210817'
+
 __all__ = ['Config', 'Policy', 'PolicySet', 'list_repo', 'fix_repo']
